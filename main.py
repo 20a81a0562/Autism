16c3bc26c7edef6d27f2fe8b3f4181c0f1ad8a54
Prasanth kumar <110885852+20a81a0562@users.noreply.github.com>
2024-01-23 10:35:54 +0530

    Add files via upload
    initial upload

---
+++ main.py
@@ -0,0 +1,49 @@
+"""This is the main module to run the app"""
+
+# Importing the necessary Python modules.
+import streamlit as st
+
+# Import necessary functions from web_functions
+from web_functions import load_data
+
+# Import pages
+from Tabs import home, data, predict
+
+
+def autism_detector_app():
+    # Configure the app
+    st.set_page_config(
+        page_title = 'Autism Level Detector',
+        page_icon = 'heavy_exclamation_mark',
+        layout = 'wide',
+        initial_sidebar_state = 'auto'
+    )
+
+    # Dictionary for pages
+    Tabs = {
+        "Home": home,
+        "Data Info": data,
+        "Prediction": predict,
+
+
+    }
+
+    # Create a sidebar
+    # Add title to sidear
+    st.sidebar.title("Navigation")
+
+    # Create radio option to select the page
+    page = st.sidebar.radio("Pages", list(Tabs.keys()))
+
+    # Loading the dataset.
+    df, X, y = load_data()
+
+    # Call the app funciton of selected page to run
+    if page in ["Prediction"]:
+        Tabs[page].app(df, X, y)
+    elif (page == "Data Info"):
+        Tabs[page].app(df)
+    else:
+        Tabs[page].app()
+if __name__ == "__main__":
+    autism_detector_app()
